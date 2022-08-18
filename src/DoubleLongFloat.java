b7f8afb775e9cb23b97f4e7342148ba0242ca7c3
Orlando <pereztwinning@gmail.com>
2022-08-18 14:18:34 -0400

    Practice 2

---
+++ src/DoubleLongFloat.java
@@ -0,0 +1,14 @@
+public class DoubleLongFloat {
+    public static void main(String[] args) {
+
+        int a = 9;
+        long b = 99999999L;
+        float c = 2.5f;
+        double d = 343433.34343;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
